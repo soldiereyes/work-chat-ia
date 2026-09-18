5bf4e3d1f3a048f6f78b00f79e892f9d316f9a35
Guilherme Gonzaga <55661896+soldiereyes@users.noreply.github.com>
2026-09-18 13:30:05 -0300

    chore: initialize backend/src/main/java/com/workchatia/WorkChatIaApplication.java

---
+++ backend/src/main/java/com/workchatia/WorkChatIaApplication.java
@@ -0,0 +1,7 @@
+package com.workchatia;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class WorkChatIaApplication {
+ public static void main(String[] args){SpringApplication.run(WorkChatIaApplication.class,args);}
+}
